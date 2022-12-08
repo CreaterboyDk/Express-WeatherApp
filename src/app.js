3ad213e991a97239479561a73f64a5e34c5d6c2d
Deepak Kumar <deepaksaw7003@gmail.com>
2022-12-08 08:14:27 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+const path = require("path");
+const port = process.env.PORT || 5000;
+const static_path = path.join(__dirname, '../public');
+const template_path = path.join(__dirname, '../templates/views');
+const partials_path = path.join(__dirname, '../templates/partials');
+app.set('views', path.join(__dirname, '../templates/views'));
+
+app.set('view engine', 'hbs');
+app.set('views', template_path);
+hbs.registerPartials(partials_path);
+
+app.use(express.static(static_path));
+
+app.get('/',(req, res)=>{
+    res.render("index")
+})
+app.get("/about",(req, res)=>{
+    res.render("about")
+})
+app.get("/weather",(req, res)=>{
+    res.render("weather")
+})
+app.get("*",(req, res)=>{
+    res.render("404page",{
+        errorMsg: "Oops! page not found, Click Here to go back"
+    })
+})
+
+app.listen(port, ()=>{
+
+    console.log(`This is running at server ${port}`)});
